a0f596314a6a06b7823a164afccdbd965ce024fb
Jose <josman185@gmail.com>
2020-05-01 10:10:29 -0500

    creating develop branch

--- NewCapitalSocial/ViewController.swift
+++ NewCapitalSocial/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // creating develop branch
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
