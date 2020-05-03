f2d111db29f6e659dd4178d7ed874e76c25e946d
Jose <josman185@gmail.com>
2020-05-03 18:33:20 -0500

    Merge gmail branch into develop

--- NewCapitalSocial/ViewController.swift
+++ NewCapitalSocial/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         // creating gmai branch
         // craeting hotm branc
     // merge hotm into develop
+    // merge gmai into develop
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
