1c9b79f322bb73f4c764d79523556f371a62bdef
Jose <josman185@gmail.com>
2020-05-04 15:47:21 -0500

    Workin with Login AutoLayout, adding some images assets and setting IBOutlets.

---
+++ NewCapitalSocial/LoginViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  NewCapitalSocial
+//
+//  Created by Jose Vargas on 26/04/20.
+//  Copyright © 2020 joscompany. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController, UITextFieldDelegate {
+    // creating develop branch
+        // creating gmai branch
+        // craeting hotm branc
+    // merge hotm into develop
+    // merge gmai into develop
+    
+    //MARK :- IBOutlets
+    
+    @IBOutlet weak var usuarioTxtF: UITextField!
+    @IBOutlet weak var contraTxtF: UITextField!
+    var activeField: UITextField?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.usuarioTxtF.delegate = self
+        self.contraTxtF.delegate = self
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        activeField = textField
+        //lastOffset = self.scrollView.contentOffset
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //activeField?.resignFirstResponder()
+        //activeField = nil
+        return true
+    }
+}
+
+extension LoginViewController {
+    
+}
+
